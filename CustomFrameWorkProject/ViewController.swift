df36a45c7d0cc47b1e0971606ae7a6b3461bed01
Ravi Chandran <ravi.chandran@tringapps.com>
2024-08-12 15:43:05 +0530

    First commit

---
+++ CustomFrameWorkProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomFrameWorkProject
+//
+//  Created by Ravi Chandran on 12/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Custom Framework project")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
